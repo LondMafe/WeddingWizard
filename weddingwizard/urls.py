f89d1e4279ee5e9546926138bda99d08ada8f0aa
LondMafe <m.londono1@utp.edu.co>
2024-04-17 15:13:45 -0500

    Logout configured

--- weddingwizard/urls.py
+++ weddingwizard/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.auth import views as auth_views
 
 from django.contrib import admin
 from django.urls import path, include
@@ -30,4 +30,7 @@ urlpatterns = [
     path('items/', include('item.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
