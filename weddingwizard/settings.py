804a4ee68efb589594c2ba55cd9c11fb152b5126
LondMafe <m.londono1@utp.edu.co>
2024-04-18 12:22:49 -0500

    fixed pics bug

--- weddingwizard/settings.py
+++ weddingwizard/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Define the directories where Django will look for static files
 STATICFILES_DIRS = [
